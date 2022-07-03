0c3578ebe52028434cb6d89be4f1e8951b3d6429
vrusali <101049802+vrusali@users.noreply.github.com>
2022-07-03 21:37:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var http = require('http')
+const express = require('express');
+const app = express();
+let port = process.env.PORT || 3000;
+const importData = require('./data.json');
 
-var dt = require('/modules');
-var server = http.createServer(function(req,res){
-    res.writeHead(200,{'content-type':'text/html'});
-    const result = dt.datetime();
-    res.write('current date and time');
-    res.write(result);
-    res.end()
-})
+app.get("/",(req,res )=> {
+res.send('hello world');
+});
+
+app.get("/players",(req,res) =>{
+    res.send(importData);
+});
+
+app.listen(port, () => {
+    console.log('example app is listening on port http://localhost:${port}');
+});
